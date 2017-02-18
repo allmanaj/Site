7f39008dec356c07fba692745eb2cced53b847ac
allmanaj <aa828@kent.ac.uk>
2017-02-18 23:41:02 +0000

    made changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,26 @@
 class ProjectsController < ApplicationController
   def index
+  	@projects = Project.all
   end
 
   def create
+  	@project = Project.create(project_params)
+  	if(@project.save)
+  		redirect_to root_url, :notice => "Project added"
+  	else
+  		render "new"
+  	end
   end
 
   def new
+  	@project = Project.new
   end
 
 
   private 
 
   def project_params
-  	
+  	params.require(:project).permit(:title, :desc, :dates)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'projects/index'
+
+  root 'projects#index'
+  resources :projects
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
